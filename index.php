84143b367da77add962d536c6e2454b53911c41e
haidoe <haidren.amalia@gmail.com>
2023-02-11 23:52:33 -0800

    Page 404 Update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<?php if(is_search(  )): ?>
+<?php if( is_search() ): ?>
   <div class="search-results-heading-meta-wrapper">
     <h2 class="search-results-title">
       Search Results
@@ -26,9 +26,29 @@
   </div>
 <?php endif; ?>
 
+<?php if( is_404() ): ?>
+  <div class="error-page-wrapper">
+    <div class="error-page-contents">
+      <h2 class="error-page-title">
+        Our apologies: 404 Error.
+      </h2>
+
+      <p>Sorry, we can't seem to find what you're looking for.</p>
+      
+      <p>
+        Try searching or go to 
+        <a href="<?php echo home_url(); ?>">
+          the homepage.
+        </a>
+      </p>
+
+      <?php get_search_form(); ?>
+    </div>
+  </div>
+<?php endif; ?>
+
 <?php
   if (have_posts()):
-
     while (have_posts()):
       the_post();
 
@@ -36,9 +56,6 @@
     endwhile;
 
     the_posts_navigation();
-    
-  else:
-    // echo "we have no posts";
   endif;
 
   get_footer();
